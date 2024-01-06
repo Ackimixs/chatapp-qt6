3dca6e2f04ac0917e95baae17df9716844a7af61
ackimixs <ackimixs@protonmail.com>
2024-01-06 16:45:50 +0100

    api structure & error handling

--- ApiClient.h
+++ ApiClient.h
@@ -8,23 +8,39 @@ class ApiClient : public QObject {
     Q_OBJECT
 
 public:
+    struct EndpointsMap
+    {
+        QString createRoom = "/api/room/create";
+        QString joinRoom = "/api/room/join";
+        QString leaveRoom = "/api/room/leave";
+        QString listRooms = "/api/room/list";
+        QString historyRoom = "/api/room/history";
+    } endpoints;
+
+
     explicit ApiClient(QObject *parent = nullptr) : QObject(parent) {
         manager = new QNetworkAccessManager(this);
     }
 
     void fetchData(const QUrl &url, const std::function<void(const QString&)>& func = nullptr) {
         qInfo() << "Fetching data from" << url;
-        QNetworkRequest request(url);
+        const QNetworkRequest request(url);
         QNetworkReply *reply = manager->get(request);
 
         connect(reply, &QNetworkReply::finished, [=]() {
             if (reply->error() == QNetworkReply::NoError) {
-                QByteArray responseData = reply->readAll();
+                const QByteArray responseData = reply->readAll();
                 qInfo() << "Response:" << responseData;
-                if (func != nullptr)
+
+                if (responseData.startsWith("error"))
+                {
+                    emit errorOccurred(responseData);
+                }
+                else if (func != nullptr)
                 {
                     func(responseData);
                 }
+
             } else {
                 qInfo() << "Error:" << reply->errorString();
                 emit errorOccurred(reply->errorString());
